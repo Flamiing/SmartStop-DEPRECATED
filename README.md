baf72321454b88aca68696ea7729f0f2dddf183e
ZumoOfZumos <amirlaaouam@gmail.com>
2022-10-18 06:31:18 +0200

    Smart Stop v0.3

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Smart Stop v0.2
+# Smart Stop v0.3
 A simple program to stop WSL from the Linux Terminal by typing 'stop'.
--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# |SMART STOP v0.2|
+# |SMART STOP v0.3|
 # Developed by: https://github.com/ZumoOfZumos
 # This program lets the user close WSL with just a simple 'stop' in the Linux Terminal
 # If needed or wanted there are several options that can be modified with parameters so that the programs suits your needs
@@ -12,12 +12,12 @@ if [[ $1 == '-t' ]] || [[ $1 == '-T' ]]; then
     # Checks if is true
     if [[ $2 == "true" ]]; then 
         # Modifyes the config file with the user preference
-        sed -i "s/WINTERMINAL=.*/WINTERMINAL=$2/g" settings.conf
+        sudo sed -i "s/WINTERMINAL=.*/WINTERMINAL=$2/g" /etc/Smart\ Stop/stop.conf
         echo "The terminal will be closed when 'stop' is used."
     # Checks if is false
     elif [[ $2 == "false" ]]; then
         # Modifyes the config file with the user preference
-        sed -i "s/WINTERMINAL=.*/WINTERMINAL=$2/g" settings.conf
+        sudo sed -i "s/WINTERMINAL=.*/WINTERMINAL=$2/g" /etc/Smart\ Stop/stop.conf
         echo "The terminal will NOT be closed when 'stop' is used."
     # If lacks option reminds the user of the usage
     else
@@ -28,7 +28,7 @@ elif [[ $1 == '-d' ]] || [[ $1 == '-D' ]]; then
     # Checks if user inputted a preference  
     if [ ! -z $2 ]; then 
         # Modifyes the config file with the user preference
-        sed -i "s/DISTRO=.*/DISTRO=$2/g" settings.conf
+        sudo sed -i "s/DISTRO=.*/DISTRO=$2/g" /etc/Smart\ Stop/stop.conf
         echo "Distro has been changed to $2"
     # If lacks option reminds the user of the usage
     else
@@ -39,7 +39,7 @@ elif [[ $1 == '-i' ]] || [[ $1 == '-I' ]]; then
     # Checks if user inputted a preference
     if [ ! -z $2 ]; then 
         # Modifyes the config file with the user preference
-        sed -i "s/EDITOR=.*/EDITOR=$2/g" settings.conf
+        sudo sed -i "s/EDITOR=.*/EDITOR=$2/g" /etc/Smart\ Stop/stop.conf
         echo "IDE has been changed to $2 "
     # If lacks option reminds the user of the usage
     else
@@ -47,12 +47,12 @@ elif [[ $1 == '-i' ]] || [[ $1 == '-I' ]]; then
     fi
 # Checks if the user is using the parameter to get help
 elif [[ $1 == '-?' ]] || [[ $1 == "--help" ]]; then
-    echo | echo; cat help.txt; echo; echo
+    echo | echo; cat /etc/Smart\ Stop/help.txt; echo; echo
 # Checks if user's input is a supported paramenter
 elif [ ! -z $1 ]; then
     echo "ERROR: Parameter not accepted"
     echo "To change the settings try using the available parameters"
-    echo "that you can check with 'stop -?' or 'stop --help'."
+    echo "that you can check with '-?' or '--help'."
 # If the code has no parameter runs the Powershell Script to close WSL
 else
     # Promt the user with a message and runs shutdown.ps1 script
