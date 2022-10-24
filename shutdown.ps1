5af66cf28174c4c554785f979a060059d87f45b6
ZumoOfZumos <amirlaaouam@gmail.com>
2022-10-24 11:05:24 +0200

    Error Handling for shutdown.ps1

--- shutdown.ps1
+++ shutdown.ps1
@@ -7,49 +7,65 @@
 # For more info please visit: https://github.com/Flamiing/SmartStop
 
 
-# Gets the settings from the conig file
-Foreach ($i in $(Get-Content ../../../etc/"Smart Stop"/settings.conf))
+try
 {
-    Set-Variable -Name $i.split("=")[0] -Value $i.split("=",2)[1]
+    # Gets the settings from the conig file
+    Foreach ($i in $(Get-Content ../../../etc/"Smart Stop"/settings.conf))
+    {
+        Set-Variable -Name $i.split("=")[0] -Value $i.split("=",2)[1]
+    }
+
+
+    # Checks for open processes and saves it to a variable
+    $IDE = Get-Process $EDITOR -ErrorAction SilentlyContinue
+    $windowsTerminal = Get-Process windowsterminal -ErrorAction SilentlyContinue
+    $openConsole = Get-Process openconsole -ErrorAction SilentlyContinue
+
+    # OPTIONAL:
+    # Checks if Windows Terminal is open and closes it
+    if ([bool]::Parse($WINTERMINAL))
+    {
+        $windowsTerminal | Stop-Process -Force 
+        $openConsole | Stop-Process -Force 
+    }
+
+    # Removes variables after use
+    Remove-Variable windowsTerminal 
+    Remove-Variable openConsole 
+    Remove-Variable WINTERMINAL 
+
+
+    # Checks if IDE is open and closes it
+    if ($IDE)
+    {
+        $IDE | Stop-Process -Force 
+    }
+
+    # Removes variables after use
+    Remove-Variable IDE 
+    Remove-Variable EDITOR 
+
+
+    # Stops the distro
+    wsl --terminate $DISTRO 
+
+    # Removes variable after use
+    Remove-Variable DISTRO 
+
+    # After all the apps using WSL are closed it turns it off and exits
+    wsl --shutdown 
 }
-
-
-# Checks for open processes and saves it to a variable
-$IDE = Get-Process $EDITOR -ErrorAction SilentlyContinue
-$windowsTerminal = Get-Process windowsterminal -ErrorAction SilentlyContinue
-$openConsole = Get-Process openconsole -ErrorAction SilentlyContinue
-
-# OPTIONAL:
-# Checks if Windows Terminal is open and closes it
-if ([bool]::Parse($WINTERMINAL))
+catch 
 {
-    $windowsTerminal | Stop-Process -Force
-    $openConsole | Stop-Process -Force
+    # Goes to the Documents path
+    Set-Location ([Environment]::GetFolderPath("MyDocuments"))
+
+    # If Smart Stop folder does not exist is created
+    if (-not (Test-Path 'Smart Stop'))
+    {     
+        New-Item -Path 'Smart Stop' -ItemType Directory
+    }
+
+    # Writes errors into a log file
+    $Error > 'Smart Stop/error.log'
 }
-
-# Removes variables after use
-Remove-Variable windowsTerminal
-Remove-Variable openConsole
-Remove-Variable WINTERMINAL
-
-
-# Checks if IDE is open and closes it
-if ($IDE)
-{
-    $IDE | Stop-Process -Force
-}
-
-# Removes variables after use
-Remove-Variable IDE
-Remove-Variable EDITOR
-
-
-# Stops the distro
-wsl --terminate $DISTRO
-
-# Removes variable after use
-Remove-Variable DISTRO
-
-
-# After all the apps using WSL are closed it turns it off and exits
-wsl --shutdown
