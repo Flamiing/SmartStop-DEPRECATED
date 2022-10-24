413299945b59b12a0e773bae4ebaa7c37e1fd6b2
ZumoOfZumos <amirlaaouam@gmail.com>
2022-10-24 12:26:14 +0200

    Smart Stop v0.8.0-preview
    This version includes a change in the shutdown file to handle errors and write them to a log file in /Documents/Smart Stop/error.log and the implementation of a switch for smart-stop.sh file

---
+++ Smart_Stop_0.8.0-preview_amd64/usr/local/Smart Stop/shutdown.ps1
@@ -0,0 +1,71 @@
+# Developed by: https://github.com/Flamiing
+# Poweshell Script to turn off WSL and all the programs using it
+
+# IMPORTANT:
+# This script will close your desired IDE and Linux Distribution
+# It will close Windows Terminal if set to 'true' in the config file
+# For more info please visit: https://github.com/Flamiing/SmartStop
+
+
+try
+{
+    # Gets the settings from the conig file
+    Foreach ($i in $(Get-Content ../../../etc/"Smart Stop"/settings.conf))
+    {
+        Set-Variable -Name $i.split("=")[0] -Value $i.split("=",2)[1]
+    }
+
+
+    # Checks for open processes and saves it to a variable
+    $IDE = Get-Process $EDITOR -ErrorAction SilentlyContinue
+    $windowsTerminal = Get-Process windowsterminal -ErrorAction SilentlyContinue
+    $openConsole = Get-Process openconsole -ErrorAction SilentlyContinue
+
+    # OPTIONAL:
+    # Checks if Windows Terminal is open and closes it
+    if ([bool]::Parse($WINTERMINAL))
+    {
+        $windowsTerminal | Stop-Process -Force 
+        $openConsole | Stop-Process -Force 
+    }
+
+    # Removes variables after use
+    Remove-Variable windowsTerminal 
+    Remove-Variable openConsole 
+    Remove-Variable WINTERMINAL 
+
+
+    # Checks if IDE is open and closes it
+    if ($IDE)
+    {
+        $IDE | Stop-Process -Force 
+    }
+
+    # Removes variables after use
+    Remove-Variable IDE 
+    Remove-Variable EDITOR 
+
+
+    # Stops the distro
+    wsl --terminate $DISTRO 
+
+    # Removes variable after use
+    Remove-Variable DISTRO 
+
+    # After all the apps using WSL are closed it turns it off and exits
+    wsl --shutdown 
+}
+catch 
+{
+    # Goes to the Documents path
+    Set-Location ([Environment]::GetFolderPath("MyDocuments"))
+
+    # If Smart Stop folder does not exist is created
+    if (-not (Test-Path 'Smart Stop'))
+    {     
+        New-Item -Path 'Smart Stop' -ItemType Directory
+    }
+
+    # Writes errors into a log file
+    $Error > 'Smart Stop/error.log'
+}
--- smart-stop.sh
+++ smart-stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# |SMART STOP v0.7.2-preview|
+# |SMART STOP v0.8.0-preview|
 # Developed by: https://github.com/Flamiing
 # This program lets the user close WSL by just typing 'smart-stop' in the Linux Terminal
 # If needed or wanted there are several options that can be modified with parameters so that the programs suits your needs
@@ -8,73 +8,90 @@
 
 
 # OPTIONS:
-# Checks if user wants to turn on/off the Windows Terminal option
-if [[ $1 == '-t' ]] || [[ $1 == '-T' ]]; then
-    # Checks if is true
-    if [[ $2 == "true" ]]; then
-        # Modifyes the config file with the user preference
-        sudo sed -i "s/WINTERMINAL=.*/WINTERMINAL=$2/g" /etc/Smart\ Stop/settings.conf
-        echo "The terminal will be closed when Smart Stop is used."
-    # Checks if is false
-    elif [[ $2 == "false" ]]; then
-        # Modifyes the config file with the user preference
-        sudo sed -i "s/WINTERMINAL=.*/WINTERMINAL=$2/g" /etc/Smart\ Stop/settings.conf
-        echo "The terminal will not be closed when Smart Stop is used."
-    # If lacks option reminds the user of the usage
-    else
-        echo "Please enter 'true' or 'false' after the '-t' parameter."
-    fi
-# Checks if user wants to change the DISTRO
-elif [[ $1 == '-d' ]] || [[ $1 == '-D' ]]; then
-    # Checks if user inputted a preference
-    if [ ! -z $2 ]; then
-        # Modifyes the config file with the user preference
-        sudo sed -i "s/DISTRO=.*/DISTRO=$2/g" /etc/Smart\ Stop/settings.conf
-        echo "Distro has been changed to $2"
-    # If lacks option reminds the user of the usage
-    else
-        echo "Please enter a distro process name after '-d' parameter."
-    fi
-# Checks if user wants to change the IDE
-elif [[ $1 == '-i' ]] || [[ $1 == '-I' ]]; then
-    # Checks if user inputted a preference
-    if [ ! -z $2 ]; then
-        # Modifyes the config file with the user preference
-        sudo sed -i "s/EDITOR=.*/EDITOR=$2/g" /etc/Smart\ Stop/settings.conf
-        echo "IDE has been changed to $2 "
-    # If lacks option reminds the user of the usage
-    else
-        echo "Please enter an IDE process name after '-d' parameter."
-    fi
-# Checks if the user is using the parameter to get help
-elif [[ $1 == '-?' ]] || [[ $1 == "--help" ]]; then
-    echo | echo; cat /etc/Smart\ Stop/help.txt; echo; echo
-# Checks version of the program
-elif [[ $1 == "--version" ]]; then
-    echo 
-    echo "Smart Stop V0.7.2-preview"
-    echo 
-    echo "MIT License"
-    echo 
-    echo "Copyright (c) 2022 Flamiing"
-    echo
-# Uninstalls the program
-elif [[ $1 == "--uninstall" ]]; then
-    echo "Uninstalling Smart Stop..."
-    sudo rm -rf /usr/bin/smart-stop
-    sudo dpkg -P smart-stop
-# Checks if user's input is a supported paramenter
-elif [ ! -z $1 ]; then
-    echo "ERROR: Parameter not accepted"
-    echo "To change the settings try using the available parameters"
-    echo "that you can check with '-?' or '--help'."
-# If the code has no parameter runs the Powershell Script to close WSL
-else
-    # Promt the user with a message and runs shutdown.ps1 script
-    echo "Turning Off"
-    sleep 0.4
+# Checks if the user wants to change the settings or turn off WSL
+case $1 in 
 
-    # Runs Powershell Script to close WSL
-    cd /usr/local/Smart\ Stop/
-    powershell.exe -f link.ps1; exit
-fi
+    # Checks if user wants to turn on/off the Windows Terminal option
+    '-t' | '-T')
+        if [[ $2 == "true" ]]; then
+            # Modifyes the config file with the user preference
+            sudo sed -i "s/WINTERMINAL=.*/WINTERMINAL=$2/g" /etc/Smart\ Stop/settings.conf
+            echo "The terminal will be closed when Smart Stop is used."
+        # Checks if is false
+        elif [[ $2 == "false" ]]; then
+            # Modifyes the config file with the user preference
+            sudo sed -i "s/WINTERMINAL=.*/WINTERMINAL=$2/g" /etc/Smart\ Stop/settings.conf
+            echo "The terminal will not be closed when Smart Stop is used."
+        # If lacks option reminds the user of the usage
+        else
+            echo "Please enter 'true' or 'false' after the '-t' parameter."
+        fi
+        ;;
+
+    # Checks if user wants to change the DISTRO
+    '-d' | '-D')
+        # Checks if user inputted a preference
+        if [ ! -z $2 ]; then
+            # Modifyes the config file with the user preference
+            sudo sed -i "s/DISTRO=.*/DISTRO=$2/g" /etc/Smart\ Stop/settings.conf
+            echo "Distro has been changed to $2"
+        # If lacks option reminds the user of the usage
+        else
+            echo "Please enter a distro process name after '-d' parameter."
+        fi
+        ;;
+
+    # Checks if user wants to change the IDE
+    '-i' | '-I')
+        # Checks if user inputted a preference
+        if [ ! -z $2 ]; then
+            # Modifyes the config file with the user preference
+            sudo sed -i "s/EDITOR=.*/EDITOR=$2/g" /etc/Smart\ Stop/settings.conf
+            echo "IDE has been changed to $2 "
+        # If lacks option reminds the user of the usage
+        else
+            echo "Please enter an IDE process name after '-d' parameter."
+        fi
+        ;;
+
+    # Checks if the user is using the parameter to get help
+    '-?' | '--help')
+        echo; cat /etc/Smart\ Stop/help.txt; echo; echo
+        ;;
+    
+    # Checks version of the program
+    '--version')
+        echo 
+        echo "Smart Stop V0.8.0-preview"
+        echo 
+        echo "MIT License"
+        echo 
+        echo "Copyright (c) 2022 Flamiing"
+        echo
+        ;;
+    
+    # Uninstalls the program
+    '--uninstall')
+        echo "Uninstalling Smart Stop..."
+        sudo rm -rf /usr/bin/smart-stop
+        sudo dpkg -P smart-stop
+        ;;
+
+    *)
+        # Checks if user's input is a supported paramenter
+        if [ ! -z $1 ]; then
+            echo "ERROR: Parameter not accepted"
+            echo "To change the settings try using the available parameters"
+            echo "that you can check with '-?' or '--help'."
+        # If the code has no parameter runs the Powershell Script to close WSL
+        else
+            # Promt the user with a message and runs shutdown.ps1 script
+            echo "Turning Off"
+            sleep 0.4
+
+            # Runs Powershell Script to close WSL
+            cd /usr/local/Smart\ Stop/
+            powershell.exe -f link.ps1; exit
+        fi
+        ;;
+esac
